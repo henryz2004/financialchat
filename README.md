6ca1f6cea3de68fcdd2f353b9964d051631831e4
henryz2004 <henryz2004@gmail.com>
2024-01-12 17:40:16 -0500

    restructured app directory

---
+++ README.md
@@ -0,0 +1,5 @@
+# This repository contains the code for the Financial Analyst chatbot for Mithril Security
+
+## How to run
+
+The chatbot is deployed via streamlit 
--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,12 @@ from selenium.webdriver.chrome.options import Options
 from urllib.parse import urlencode
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_prefs = {}
+chrome_options.experimental_options["prefs"] = chrome_prefs
+chrome_prefs["profile.default_content_settings"] = {"images": 2}
 driver = webdriver.Chrome(options=chrome_options)
 
 prompt = """
