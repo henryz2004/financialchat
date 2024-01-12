d6b39a5e591fc7002f78fa6e8ec64f6d91a1121d
henryz2004 <henryz2004@gmail.com>
2024-01-12 17:17:00 -0500

    first commit

---
+++ chatbot.py
@@ -0,0 +1,94 @@
+from openai import OpenAI
+import utils as ut
+import streamlit as st
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key="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")
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4-1106-preview"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "questions" not in st.session_state:
+    st.session_state.questions = []
+if "backend_thread" not in st.session_state:
+    st.session_state.backend_thread = [
+        {
+        "role": "system",
+        "content": ut.prompt
+        },
+    ]
+if "fetch" not in st.session_state:
+    st.session_state.fetch = True
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.questions.append(prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+
+        ## Process the query, fetch FT articles, and prompt the agent
+        ## Also display the links to the FT articles and update the status of fetching the articles
+        rag = ""
+        if st.session_state.fetch:
+            with st.status("Finding up-to-date information [Click to view details]..."):
+
+                st.write("Generating query...")
+                query = client.chat.completions.create(
+                    model=st.session_state["openai_model"],
+                    messages=[
+                        {"role":"system", "content":ut.prompt},
+                        {"role":"user", "content":ut.generate_query_prompt(prompt, st.session_state.questions)}
+                    ],
+                    max_tokens=64,
+                    stop=["\n"],
+                    stream=False,
+                ).choices[0].message.content.strip().replace('"', '')
+                
+                search_url = ut.get_ft_search_url(query)
+                st.write(f"Searching the Financial Times for {query}...")
+                ft_urls = ut.get_ft_urls(search_url)
+                art_infos = []
+                st.write("Fetching articles...")
+                for art_info in ut.get_article_infos(ft_urls):
+                    st.write(f"Fetched article: {art_info['title']}.")
+                    art_infos.append(art_info)
+                rag = ut.generate_article_rag(art_infos)
+                st.write("Prompting the agent")
+
+        backend_prompt = ut.generate_prompt(prompt, rag)
+
+        st.session_state.backend_thread.append(
+            {"role": "user", "content": backend_prompt}
+        )
+        print(backend_prompt)
+
+
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.backend_thread
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.backend_thread.append({
+        "role": "assistant",
+        "content": full_response
+    })
+
+st.toggle("Toggle article fetching", value=st.session_state.fetch, on_change=lambda: st.session_state.update({"fetch": not st.session_state.fetch}))
---
+++ utils.py
@@ -0,0 +1,101 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from urllib.parse import urlencode
+
+chrome_options = Options()
+#chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=chrome_options)
+
+prompt = """
+You are a financial advisor who helps users with analyzing the current state of the market and economy. It is critical that your answers be factual and nuanced.
+"""
+
+N_PARA_BEGIN = 7
+N_PARA_END = 7
+def grab_article_info(ft_url):
+
+    try:
+    
+        q_string = urlencode({"url": ft_url})
+        archive_url_req = "https://archive.ph/submit/?"+q_string
+        driver.get(archive_url_req)
+        
+        title = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#o-topper > h1 > span"))
+        ).text
+    
+        body = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "article-body"))
+        )
+    
+        all_text = body.text #all_text = get_text_recursive(body)
+        paragraphs = all_text.split("\n")
+        trunc = []
+        if len(paragraphs) <= N_PARA_BEGIN + N_PARA_END:
+            trunc = paragraphs
+        else:
+            trunc = paragraphs[:N_PARA_BEGIN] + paragraphs[-N_PARA_END:]
+        
+        return {"title": title, "short":trunc}
+
+    except TimeoutException as e:
+        return {"title": "", "short":""}
+    
+def get_ft_search_url(query):
+    query_string = urlencode({"sort":"relevance", "q":query})
+    return f"https://www.ft.com/search?{query_string}"
+
+def get_ft_urls(search_url):
+    driver.get(search_url)
+    search_results = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#site-content > div.search-results.o-teaser-collection > ul"))
+    )
+    
+    header_links = search_results.find_elements(By.CLASS_NAME, "js-teaser-heading-link")
+    
+    ft_urls = []
+    for res in header_links:
+        ft_url = res.get_attribute("href")
+        ft_urls.append(ft_url)
+    return ft_urls
+
+def get_article_infos(ft_urls, k=5):
+    for i, ft_url in enumerate(ft_urls[:k]):
+        print(f"fetching article {i+1}/{k}")
+        info = grab_article_info(ft_url)
+        yield info
+    
+
+def generate_article_rag(art_infos):
+    ret = ""
+    for art in art_infos:
+        ret += "Title: "+art['title']+"\n"
+        ret += "Excerpt:"+'\n'.join(art['short'])+"\n\n"
+    return ret
+
+def generate_prompt(question, rag):
+    ret = question
+    ret += "\n\n###\n\n"
+    ret += "Here are some recent article snippets from the Financial Times found by searching up the question that can help inform your response:\n\n"
+    ret += rag
+
+    return ret
+
+def generate_query_prompt(prompt, questions):
+    formatted_questions = "\n".join(questions)
+    return f"""
+What is a good query to find useful articles on the Financial Times about the question:
+
+"{prompt}"
+
+Previous questions the user asked are:
+
+{formatted_questions}
+
+###
+
+Please answer with just the search query that would be entered into the FT searchbar. For example, a good response for "How is the us economy doing?" is "US economy" with no quotation marks"""
