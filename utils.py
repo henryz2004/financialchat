8bced03d229a20abab70e8e561fe7f53ce794e5b
henryz2004 <henryz2004@gmail.com>
2024-01-12 22:44:36 -0500

    selenium

--- utils.py
+++ utils.py
@@ -7,12 +7,13 @@ from selenium.webdriver.chrome.options import Options
 from urllib.parse import urlencode
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_prefs = {}
-chrome_options.experimental_options["prefs"] = chrome_prefs
-chrome_prefs["profile.default_content_settings"] = {"images": 2}
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+#chrome_prefs = {}
+#chrome_options.experimental_options["prefs"] = chrome_prefs
+#chrome_prefs["profile.default_content_settings"] = {"images": 2}
 driver = webdriver.Chrome(options=chrome_options)
 
 prompt = """
